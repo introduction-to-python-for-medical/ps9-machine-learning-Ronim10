d75f98a2f7158ed238021a3d2aae7e3894a554b2
Ronim10 <Ronim2122@gmail.com>
2025-02-21 14:03:30 +0200

    Update main.py

--- main.py
+++ main.py
@@ -15,16 +15,10 @@ x_scaled = scaler.fit_transform(x)
 from sklearn.model_selection import train_test_split
 x_train, x_test, y_train, y_test = train_test_split(x_scaled, y, test_size=0.2, random_state=42)
 
-from sklearn.svm import SVC
-svc = SVC ()
-svc.fit(x_train, y_train)
+from sklearn.neighbors import KNeighborsClassifier
 
+model = KNeighborsClassifier()
+model.fit(x_train, y_train)
+y_perd= model.predict(x_test)
 from sklearn.metrics import accuracy_score
-y_pred = svc.predict(x_test)
-accuracy = accuracy_score(y_test, y_pred)
-print(f"Accuracy: {accuracy}")
-
-
-import joblib
-
-joblib.dump(svc, 'svc_model.joblib')
+accuracy_score(y_test,y_perd)
